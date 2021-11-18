bdc896a8d45ea0d4647f34e91fcd7edfe9495e30
Matheus Fernandes <sd1057bernardo@live.com>
2021-11-18 19:15:29 -0300

    create index.js and started npm project

---
+++ index.js
@@ -0,0 +1,18 @@
+console.clear();
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get("/",(req,res)=>{
+	res.render("index");
+});
+
+
+app.listen(port, ()=>{
+	console.log(`Servidor iniciado na porta ${port}`);
+});
+
